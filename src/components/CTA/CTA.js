a940fb7d8ebdadff655faa2087e4ebb7d53f2c59
Zenia Gist <zg@buildmidwestern.com>
2021-11-30 11:16:34 -0600

    added cta images and text

---
+++ src/components/CTA/CTA.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+import {
+  StyledImg,
+  CTAImage,
+  CTAImageTextWrapper,
+  CTAImageText,
+} from "./CTA.styles"
+
+const CTA = ({ image, link, text }) => {
+  return (
+    <CTAImage>
+      <StyledImg image={image} alt="CTA Image" />
+      <Link to={link}>
+        <CTAImageTextWrapper>
+          <CTAImageText>{text}</CTAImageText>
+        </CTAImageTextWrapper>
+      </Link>
+    </CTAImage>
+  )
+}
+
+export default CTA
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/SEO/SEO"
 import Hero from "../components/Hero/Hero.js"
+import CTAArea from "../components/CTAArea/CTAArea.js"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <CTAArea />
   </Layout>
 )
 
